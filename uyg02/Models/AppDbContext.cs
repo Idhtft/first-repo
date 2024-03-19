e34bb2e897f297221249f0fb1f2d5ab5558e379c
Taha Morgan <tahaaydin17@gmail.com>
2024-03-19 18:35:59 +0300

    first commit

---
+++ uyg02/Models/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uyg02.Models
+{
+    public class AppDbContext:DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=taha\\MSSQLSERVER01;Database=odev;Encrypt=false;Trusted_Connection=True;");
+        }
+    }
+}
